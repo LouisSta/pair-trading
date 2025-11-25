a56dad022aa93502623fd9f7a4799d5fdba3098a
LouisSta <louis.stanisiere@eleve.ensai.fr>
2025-11-25 17:09:49 +0100

    Update loader.py

--- python/loader.py
+++ python/loader.py
@@ -1,10 +1,10 @@
 import yfinance as yf
 
 
-def load_data(ticker1: str, ticker2: str):
+def load_data(ticker1: str, ticker2: str, start="2015-01-01", end="2025-01-01"):
     df = yf.download(tickers=[ticker1, ticker2],
-                     start="2015-01-01",
-                     end="2025-01-01")["Adj Close"]
+                     start=start,
+                     end=end)["Adj Close"]
     df.columns = [ticker1, ticker2]
     df.dropna(inplace=True)
     df.to_csv(f"../data/{ticker1}&{ticker2}.csv")
