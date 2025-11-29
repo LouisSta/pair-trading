acff08d09291586741d0e1d9e8895b79ee19c23a
LouisSta <louis.stanisiere@eleve.ensai.fr>
2025-11-29 14:10:17 +0100

    backtests

--- python/backtest.py
+++ python/backtest.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def PnL(df, A, B):
     df['pnl'] = df['position'] * (df[f"{A}_ret"] - df[f"{B}_ret"])
     df['pnl_cum'] = df['pnl'].cumsum()
@@ -11,18 +14,58 @@ def PnL_compound(df, A, B):
     return df
 
 
-def max_drawdown(df, col='capital'):
-    series = df[col]
-    roll_max = series.cummax()
-    dd = series / roll_max - 1
-    return dd.min()
+def sharpe_ratio(returns, risk_free_rate=0.0, freq=252):
+    excess = returns - risk_free_rate / freq
+    if returns.std() == 0:
+        return np.nan
+    return np.sqrt(freq) * excess.mean() / excess.std()
 
 
-def sharpe_ratio(df, freq=252):  # 252 days per year]
-    return (df['pnl'].mean() / df['pnl'].std()) * (freq ** 0.5)
+def sortino_ratio(returns, risk_free_rate=0.0, freq=252):
+    excess = returns - risk_free_rate / freq
+    downside_std = excess[excess < 0].std()
+
+    if downside_std == 0 or np.isnan(downside_std):
+        return np.nan
+
+    return np.sqrt(freq) * excess.mean() / downside_std
 
 
-def sortino_ratio(df, freq=252):
-    ret = df['pnl']
-    downside = ret[ret < 0]
-    return (ret.mean() / downside.std()) * (freq ** 0.5)
+def max_drawdown(capital):
+    roll_max = capital.cummax()
+    drawdown = (capital - roll_max) / roll_max
+    return drawdown.min()
+
+
+def CAGR(capital, freq=252):
+    total_periods = len(capital)
+    if total_periods == 0:
+        return np.nan
+    return (capital.iloc[-1])**(freq / total_periods) - 1
+
+
+def calmar_ratio(capital, freq=252):
+    cagr = CAGR(capital, freq)
+    mdd = abs(max_drawdown(capital))
+    if mdd == 0:
+        return np.nan
+    return cagr / mdd
+
+
+def performance_report(returns, capital, risk_free_rate=0.0, freq=252) ->\
+        dict:
+
+    report = {
+        "Sharpe Ratio": sharpe_ratio(returns, risk_free_rate, freq),
+        "Sortino Ratio": sortino_ratio(returns, risk_free_rate, freq),
+        "Max Drawdown (%)": 100 * max_drawdown(capital),
+        "CAGR (%)": 100 * CAGR(capital, freq),
+        "Calmar Ratio": calmar_ratio(capital, freq),
+        "Volatility (%)": 100 * returns.std() * np.sqrt(freq),
+        "Annualized Return (%)": 100 * returns.mean() * freq,
+        "Maximum capital": capital.max(),
+        "Minimum capital": capital.min(),
+        "Final capital": capital.iloc[-1]
+    }
+
+    return report
--- python/strategies.py
+++ python/strategies.py
@@ -2,31 +2,31 @@ import pandas as pd
 from python.spread import compute_spread
 
 
-def pairs_z_score(df, A: str, B: str, entry: float, tp: float, sl: float, window=None):
+def pairs_z_score(df, A: str, B: str, entry: float, tp: float, sl: float, window=60):
     """
     tp<entry<sl
     """
     positions = []
+    spreads = []
     position = 0
 
     for i in range(len(df)):
-        if window is None:
-            start_idx = 0
-        else:
-            start_idx = max(0, i - window + 1)
+
+        start_idx = max(0, i - window + 1)
 
         df_window = df.iloc[start_idx:i+1].copy()
 
         df_window, beta = compute_spread(df_window, A, B)
         spread_window = df_window[f"spread_{A}_{B}"]
 
+        spr_today = spread_window.iloc[-1]
         spr_mean = spread_window.mean()
         spr_std = spread_window.std()
         
         if spr_std == 0 or not isinstance(spr_std, float):
             z = 0
         else:
-            z = (spread_window.iloc[-1] - spr_mean) / spr_std
+            z = (spr_today - spr_mean) / spr_std
 
         if abs(z) < tp or abs(z) > sl:
             position = 0
@@ -38,6 +38,8 @@ def pairs_z_score(df, A: str, B: str, entry: float, tp: float, sl: float, window
             position = 1  # long spread
 
         positions.append(position)
+        spreads.append(spr_today)
 
+    df["spread"] = pd.Series(spreads)
     df["position"] = pd.Series(positions).shift(1, fill_value=0)  # trade the next day
     return df
